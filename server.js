83f2d60dd7a8eaa5471658df8fecf9adac094da2
Ryan Carey <ryaanski18@yahoo.com>
2021-12-02 21:46:44 -0500

    moved listen to the bottom and the * get to right above that

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const noteData = require("./db/db.json");
 const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-const { readFromFile, readAndAppend } = require("./helpers/fsUtils");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("./helpers/fsUtils");
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,16 +18,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
 });
 
 app.get("/api/notes", (req, res) => {
@@ -34,15 +30,39 @@ app.post("/api/notes", (req, res) => {
   const { title, text } = req.body;
 
   if (req.body) {
-    const newTip = {
+    const newNote = {
       title,
       text,
       id: uuidv4(),
     };
 
-    readAndAppend(newTip, "./db/db.json");
+    readAndAppend(newNote, "./db/db.json");
     res.json(`Tip added successfully 🚀`);
   } else {
     res.error("Error in adding tip");
   }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((tip) => tip.id !== id);
+
+      // Save that array to the filesystem
+      writeToFile("./db/db.json", result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${id} has been deleted 🗑️`);
+    });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
